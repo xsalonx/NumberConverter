4923c9059cf44809974e51a7299e7c1a2faf4915
xsalonx <lukasz.dubiel3@gmail.com>
2021-08-11 08:31:13 +0200

    made it rest

--- src/main/java/com/springTutorial/controller/RESTController.java
+++ src/main/java/com/springTutorial/controller/RESTController.java
@@ -1,35 +1,29 @@
 package com.springTutorial.controller;
 
-import com.springTutorial.model.ConversionData;
 import com.springTutorial.model.Number;
 import com.springTutorial.service.Converter;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+// TODO use Converter as singleton
 
 @RestController
 public class RESTController {
 
-    @RequestMapping(path="converter", method=RequestMethod.GET)
+    @RequestMapping(value ="converter", method=RequestMethod.GET, produces = "application/json")
     public @ResponseBody Number postConverter(@RequestParam("value") String value,
                                               @RequestParam("notationFrom") String notationFrom,
                                               @RequestParam("notationTo") String notationTo) {
-
-        System.out.println(value);
-        System.out.println(notationFrom);
-        System.out.println(notationTo);
 
         Number receivedNumber = new Number(value, notationFrom);
         if (receivedNumber.getValue() != null && !receivedNumber.getValue().equals("")) {
             Converter converter = new Converter(receivedNumber);
             try {
-                Number number = converter.convert(notationTo);
-                return number;
+                return converter.convert(notationTo);
             } catch (NumberFormatException e) {
                 return new Number(e.getMessage(), "");
             }
         }
-        System.out.println(value);;
+        System.out.println(value);
         return new Number("", "");
     }
 }
